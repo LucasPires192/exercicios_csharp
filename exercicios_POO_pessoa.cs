32a9e04c4613e489e0f800e56ba62a95edcbc7b9
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-12 22:10:55 -0300

    Exercicio para praticar POO, objeto pessoa

---
+++ exercicios_POO_pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa pessoa = new Pessoa();
+        
+        Console.WriteLine("Olá! Qual é o seu nome?");
+        
+        pessoa.Nome = Console.ReadLine();
+
+        Console.WriteLine($"Bem vindo {pessoa.Nome}! Quantos anos você tem?");
+        
+        int idade;
+
+        while(true)
+        {
+            string entrada = Console.ReadLine();
+
+            if(!int.TryParse(entrada, out idade))
+            {
+                Console.WriteLine("Idade Invalida! Tente digitar um número com a sua idade!");
+            }else
+            {   
+                pessoa.Idade = idade;
+                break;
+            }
+        }
+
+        pessoa.Apresentar();
+    }
+    
+    class Pessoa{
+        public string Nome;
+        public int Idade;
+
+        public void Apresentar(){
+            Console.WriteLine($"Prazer em te conhecer {Nome}! Então você tem {Idade} anos. Que legal");
+        }
+    }
+}
