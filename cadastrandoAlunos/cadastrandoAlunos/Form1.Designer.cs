1c2b32f334306df40fa973a63b30f0befc3eb29c
LucasPires192 <lucas.andrade192@etc.sp.gov.br>
2025-04-25 22:10:14 -0300

    Formulario simples de cadastro de aluno

---
+++ cadastrandoAlunos/cadastrandoAlunos/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace cadastrandoAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbCurso = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbManha = new System.Windows.Forms.RadioButton();
+            this.rbTarde = new System.Windows.Forms.RadioButton();
+            this.rbNoite = new System.Windows.Forms.RadioButton();
+            this.chkEmail = new System.Windows.Forms.CheckBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(71, 55);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(341, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Curso";
+            // 
+            // cbCurso
+            // 
+            this.cbCurso.FormattingEnabled = true;
+            this.cbCurso.Items.AddRange(new object[] {
+            "Administração",
+            "Desenvolvimento de sistemas",
+            "Logistica"});
+            this.cbCurso.Location = new System.Drawing.Point(71, 138);
+            this.cbCurso.Name = "cbCurso";
+            this.cbCurso.Size = new System.Drawing.Size(162, 21);
+            this.cbCurso.TabIndex = 3;
+            this.cbCurso.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Turno";
+            // 
+            // rbManha
+            // 
+            this.rbManha.AutoSize = true;
+            this.rbManha.Location = new System.Drawing.Point(71, 221);
+            this.rbManha.Name = "rbManha";
+            this.rbManha.Size = new System.Drawing.Size(58, 17);
+            this.rbManha.TabIndex = 5;
+            this.rbManha.TabStop = true;
+            this.rbManha.Text = "Manhã";
+            this.rbManha.UseVisualStyleBackColor = true;
+            // 
+            // rbTarde
+            // 
+            this.rbTarde.AutoSize = true;
+            this.rbTarde.Location = new System.Drawing.Point(219, 221);
+            this.rbTarde.Name = "rbTarde";
+            this.rbTarde.Size = new System.Drawing.Size(53, 17);
+            this.rbTarde.TabIndex = 6;
+            this.rbTarde.TabStop = true;
+            this.rbTarde.Text = "Tarde";
+            this.rbTarde.UseVisualStyleBackColor = true;
+            // 
+            // rbNoite
+            // 
+            this.rbNoite.AutoSize = true;
+            this.rbNoite.Location = new System.Drawing.Point(362, 221);
+            this.rbNoite.Name = "rbNoite";
+            this.rbNoite.Size = new System.Drawing.Size(50, 17);
+            this.rbNoite.TabIndex = 7;
+            this.rbNoite.TabStop = true;
+            this.rbNoite.Text = "Noite";
+            this.rbNoite.UseVisualStyleBackColor = true;
+            this.rbNoite.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // chkEmail
+            // 
+            this.chkEmail.AutoSize = true;
+            this.chkEmail.Location = new System.Drawing.Point(70, 286);
+            this.chkEmail.Name = "chkEmail";
+            this.chkEmail.Size = new System.Drawing.Size(134, 17);
+            this.chkEmail.TabIndex = 8;
+            this.chkEmail.Text = "Deseja receber e-mail?";
+            this.chkEmail.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(70, 323);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(129, 40);
+            this.btnCadastrar.TabIndex = 9;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(447, 323);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(341, 118);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.chkEmail);
+            this.Controls.Add(this.rbNoite);
+            this.Controls.Add(this.rbTarde);
+            this.Controls.Add(this.rbManha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbCurso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbCurso;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbManha;
+        private System.Windows.Forms.RadioButton rbTarde;
+        private System.Windows.Forms.RadioButton rbNoite;
+        private System.Windows.Forms.CheckBox chkEmail;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ cadastrandoAlunos/cadastrandoAlunos/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastrandoAlunos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string curso = cbCurso.SelectedItem?.ToString() ?? "Não selecionado";
+            string turno = "";
+
+            if (rbManha.Checked)
+                turno = "Manhão";
+            else if (rbTarde.Checked)
+                turno = "Tarde";
+            else if (rbNoite.Checked)
+                turno = "Noite";
+            else
+                turno = "Não selecionado";
+
+            string recebeEmail = chkEmail.Checked ? "Sim" : "Não";
+
+            lblResultado.Text = $"Nome: {nome}\nCurso: {curso}\nTurno: {turno}\nRecebe e-emails: {recebeEmail}";
+        }
+    }
+}
